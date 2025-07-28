270bb20b13761791704890be616e6169d1c1c8ea
matei <mateiprodan1@gmail.com>
2025-07-28 13:14:08 +0300

    47

---
+++ Solutions/47. Permutations II/main.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        backtrack(nums, used, path, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int> &nums, vector<bool> &used, vector<int> &path, vector<vector<int>> &result)
+    {
+        if (path.size() == nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (used[i])
+                continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtrack(nums, used, path, result);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};

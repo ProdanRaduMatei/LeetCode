df83c34fd687dc216a98acfe4cbe102382e001d4
matei <mateiprodan1@gmail.com>
2025-07-31 18:24:52 +0300

    242

---
+++ Solutions/242. Valid Anagram/main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+            return false;
+        vector<int> count(26, 0);
+        for (char c : s)
+            ++count[c - 'a'];
+        for (char c : t)
+            --count[c - 'a'];
+        for (int val : count)
+            if (val != 0)
+                return false;
+        return true;
+    }
+};

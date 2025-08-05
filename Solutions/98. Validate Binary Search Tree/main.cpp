0d57b76322b0bacabec30c52ede1be8a9463b200
matei <mateiprodan1@gmail.com>
2025-08-05 17:40:03 +0300

    98

---
+++ Solutions/98. Validate Binary Search Tree/main.cpp
@@ -0,0 +1,31 @@
+#include <limits.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool helper(TreeNode *node, long long minVal, long long maxVal)
+    {
+        if (!node)
+            return true;
+        if (node->val <= minVal || node->val >= maxVal)
+            return false;
+        return helper(node->left, minVal, node->val) && helper(node->right, node->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode *root)
+    {
+        return helper(root, LLONG_MIN, LLONG_MAX);
+    }
+};

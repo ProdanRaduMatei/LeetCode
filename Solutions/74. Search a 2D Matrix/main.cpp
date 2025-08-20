ac5fd00efc27da0e3a2e63d6ed0ea2b399d6b9c9
matei <mateiprodan1@gmail.com>
2025-08-20 10:46:57 +0300

    74

---
+++ Solutions/74. Search a 2D Matrix/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int lo = 0, hi = m * n - 1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            int r = mid / n;
+            int c = mid % n;
+            int val = matrix[r][c];
+            if (val == target)
+                return true;
+            else if (val < target)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return false;
+    }
+};

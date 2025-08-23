8421ed32689ff8aceced73135a79a29da1db11b3
matei <mateiprodan1@gmail.com>
2025-08-23 08:05:59 +0300

    191

---
+++ Solutions/191. Number of 1 Bits/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+        int count = 0;
+        while (n)
+        {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+};

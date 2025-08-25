18b8ff39b39affa92e3253a04ebd4a68ad7cf02f
matei <mateiprodan1@gmail.com>
2025-08-25 12:02:32 +0300

    120

---
+++ Solutions/120. Triangle/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        int n = triangle.size();
+        vector<int> dp = triangle.back();
+        for (int r = n - 2; r >= 0; --r)
+            for (int c = 0; c <= r; ++c)
+                dp[c] = triangle[r][c] + min(dp[c], dp[c + 1]);
+        return dp[0];
+    }
+};

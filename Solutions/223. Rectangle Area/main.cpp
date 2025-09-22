95816139f6fbbc8fb90588c03e0b2903d956fd08
matei <mateiprodan1@gmail.com>
2025-09-22 10:51:43 +0300

    223

---
+++ Solutions/223. Rectangle Area/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+    {
+        long long areaA = 1LL * (ax2 - ax1) * (ay2 - ay1);
+        long long areaB = 1LL * (bx2 - bx1) * (by2 - by1);
+        long long overlapW = max(0, min(ax2, bx2) - max(ax1, bx1));
+        long long overlapH = max(0, min(ay2, by2) - max(ay1, by1));
+        long long overlap = overlapW * overlapH;
+        return (int)(areaA + areaB - overlap);
+    }
+};

d2925d2d513d500941a5bc79cb5a183e46ea9ebd
matei <mateiprodan1@gmail.com>
2025-09-23 18:30:28 +0300

    237

---
+++ Solutions/237. Delete Node in a Linked List/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;   // Copy next node's value
+        node->next = node->next->next; // Bypass next node
+    }
+};
